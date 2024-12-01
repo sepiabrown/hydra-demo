ed98b9bbf6f0ec2cd652c78844c2e6a4e5deaa3e
Suwon Park <suwonpark@haedosa.xyz>
2024-12-01 16:52:55 +0900

    [fix] new test-readme

--- flake.nix
+++ flake.nix
@@ -6,20 +6,31 @@
   };
 
   outputs = { self, nixpkgs }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      now = "241211 1213";
     in
     {
 
       packages.x86_64-linux.tree = nixpkgs.legacyPackages.x86_64-linux.tree;
 
+      packages.x86_64-linux.test = pkgs.runCommand "readme" { } ''
+        echo hello world! ${now}
+        mkdir -p $out/nix-support
+        echo "# A new readme !" > $out/readme.md
+        echo "doc readme $out/readme.md" >> $out/nix-support/hydra-build-products
+      '';
+
       defaultPackage.x86_64-linux = self.packages.x86_64-linux.tree;
 
+      hydraJobs.tester5."tester6" = self.defaultPackage;
+      hydraJobs.tester3.tester4 = self.defaultPackage;
       hydraJobs."tester2" = self.defaultPackage;
       hydraJobs."tester" = self.defaultPackage;
       hydraJobs."tester-readme" = pkgs.runCommand "readme" { } ''
-        echo hello worl
+        echo hello world! ${now}
         mkdir -p $out/nix-support
-        echo "# A readme" > $out/readme.md
+        echo "# A new readme !! ${now}" > $out/readme.md
         echo "doc readme $out/readme.md" >> $out/nix-support/hydra-build-products
       '';
     };
